d4eb7f5b5a2b9c2a52fc3bca60319b36c9d903e9
nastasia-ch <nastasia-chernega@mail.ru>
2023-03-02 03:40:44 +0300

    refactor mobile tests

--- src/test/java/mailru/nastasiachernega/tests/tests/testsMobile/TranslateTests.java
+++ src/test/java/mailru/nastasiachernega/tests/tests/testsMobile/TranslateTests.java
@@ -8,7 +8,9 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byTextCaseInsensitive;
 import static com.codeborne.selenide.Selenide.*;
+import static io.appium.java_client.AppiumBy.accessibilityId;
 import static io.appium.java_client.AppiumBy.id;
 import static io.qameta.allure.Allure.step;
 
