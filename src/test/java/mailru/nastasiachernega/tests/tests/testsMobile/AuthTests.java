8e040d1850d8001e7db837f14a9c354b640dbf64
nastasia-ch <nastasia-chernega@mail.ru>
2023-03-03 13:46:09 +0300

    refactor mobile tests

--- src/test/java/mailru/nastasiachernega/tests/tests/testsMobile/AuthTests.java
+++ src/test/java/mailru/nastasiachernega/tests/tests/testsMobile/AuthTests.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.sleep;
 import static io.appium.java_client.AppiumBy.id;
 import static io.qameta.allure.Allure.step;
 
